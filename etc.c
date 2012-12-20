9174242f48ec4e760f87dfb84492339ddb052fa5
Thomas Luce <thomas.luce@gmail.com>
2012-12-20 14:16:11 -0800

    Updated the parser to build a proper instruction list so that I could add another operation that is just "go here." I would say that now, it's useful... for some definition of that word.
    What the actual fuck am I doing with my life?

--- etc.c
+++ etc.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 FILE *input_file;
+
+#define INC    0
+#define DEC    1
+#define IN     2
+#define OUT    3
+#define INC_B  4
+#define DEC_B  5
+#define SKIP   6
+#define SKIP_G 7
+#define GO     8
 
 int parse_double_dot() {
   unsigned char input = getc(input_file);
@@ -22,6 +31,51 @@ int parse_elipsis() {
   }
 }
 
+void read_program(unsigned char *instruction_buffer) {
+  char input;
+  unsigned char temp;
+  int ip = 0;
+  while((input = getc(input_file)) != EOF) {
+    switch(input) {
+      case '.':
+        if(parse_elipsis() == 1) {
+          instruction_buffer[ip] = INC;
+        } else if(parse_double_dot() == 1) {
+          instruction_buffer[ip] = DEC;
+        } else {
+          instruction_buffer[ip] = IN;
+        }
+        break;
+      case '\'':
+        instruction_buffer[ip] = OUT;
+        break;
+      case ' ':
+        temp = getc(input_file);
+        if(temp == ' ') {
+          instruction_buffer[ip] = INC_B;
+        } else {
+          ungetc(temp, input_file);
+          instruction_buffer[ip] = DEC_B;
+        }
+        break;
+      case '\n':
+        instruction_buffer[ip] = SKIP;
+        break;
+      case '\t':
+        instruction_buffer[ip] = SKIP_G;
+        break;
+      case '/':
+        instruction_buffer[ip] = GO;
+        break;
+      default:
+        fprintf(stderr, "Unknown command: %c(%d)\n", input, input);
+        exit(1);
+    }
+
+    ip++;
+  }
+}
+
 int main(int argc, char **argv) {
   if(argc > 1) {
     input_file = fopen(argv[1], "r");
@@ -29,47 +83,51 @@ int main(int argc, char **argv) {
     input_file = input_file;
   }
 
-  char input;
-  unsigned char temp;
   unsigned char memory[65535];
-  int  pointer = 0;
+  unsigned char instructions[65535];
+
+  int pointer = 0;
+  int ip = 0;
+  int instruction = 0;
   for(pointer =0; pointer < 65535; pointer++) memory[pointer] = 0;
+  for(pointer =0; pointer < 65535; pointer++) instructions[pointer] = 99;
+  read_program(instructions);
   pointer = 0;
 
-  while((input = getc(input_file)) != EOF) {
-    switch(input) {
-      case '.':
-        if(parse_elipsis() == 1) {
-          pointer++;
-        } else if(parse_double_dot() == 1) {
-          pointer--;
-        } else {
-          memory[pointer] = getc(stdin);
-        }
+  while((instruction = instructions[ip]) != 99) {
+    switch(instruction) {
+      case INC:
+        pointer++;
         break;
-      case '\'':
+      case DEC:
+        pointer--;
+        break;
+      case IN:
+        memory[pointer] = getc(stdin);
+        break;
+      case OUT:
         putc(memory[pointer], stdout);
         break;
-      case ' ':
-        temp = getc(input_file);
-        if(temp == ' ') {
-          memory[pointer] += 1;
-          break;
-        } else {
-          ungetc(temp, input_file);
-          memory[pointer] -= 1;
-        }
+      case INC_B:
+        memory[pointer] += 1;
         break;
-      case '\n':
-        if(memory[pointer] == 0) getc(input_file);
+      case DEC_B:
+        memory[pointer] -= 1;
         break;
-      case '\t':
-        if(memory[pointer] > 0) getc(input_file);
+      case SKIP:
+        if(memory[pointer] == 0) ip++;
+        break;
+      case SKIP_G:
+        if(memory[pointer] > 0) ip++;
+        break;
+      case GO:
+        ip = memory[pointer]-1;
         break;
       default:
-        fprintf(stderr, "Unknown command: %c(%d)\n", input, input);
+        fprintf(stderr, "Should not be here\n");
         return 1;
     }
+    ip++;
   }
 
   return 0;
